8ab57eea772da0806d910cba41c285a24f1a32c5
12430373 <102036037+12430373@users.noreply.github.com>
2022-04-08 01:41:52 -0400

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,11 @@ public class Main {
         Ejercicio_1 ejercicio1=new Ejercicio_1();
         ejercicio1.ejecutar();
 
-       // Ejercicio_2 ejercicio2=new Ejercicio_2();
+        //Ejercicio_2 ejercicio2=new Ejercicio_2();
         //ejercicio2.ejecutar();
+        
+        //Practica_3 ejercicio3= new Practica_3();
+        //ejercicio3.ejecutar();
+        
     }
 }
